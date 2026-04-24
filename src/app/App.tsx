acd5eed418b5dac9646688e79c50624ebd64c0fb
hundredout <david@hundredout.com>
2026-04-24 15:25:09 -0700

    Reset scroll position on route change

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { HomePage } from "./pages/HomePage";
@@ -10,23 +11,40 @@ import { ContentPage } from "./pages/ContentPage";
 import { CampaignsPage } from "./pages/CampaignsPage";
 import { DownloadPage } from "./pages/DownloadPage";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+}
+
+function AppShell() {
+  return (
+    <div className="min-h-screen">
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/app" element={<AppPage />} />
+        <Route path="/gear" element={<GearPage />} />
+        <Route path="/club" element={<ClubPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/download" element={<DownloadPage />} />
+        <Route path="/content" element={<ContentPage />} />
+        <Route path="/campaigns" element={<CampaignsPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/app" element={<AppPage />} />
-          <Route path="/gear" element={<GearPage />} />
-          <Route path="/club" element={<ClubPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/download" element={<DownloadPage />} />
-          <Route path="/content" element={<ContentPage />} />
-          <Route path="/campaigns" element={<CampaignsPage />} />
-        </Routes>
-        <Footer />
-      </div>
+      <AppShell />
     </BrowserRouter>
   );
 }
